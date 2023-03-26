fbd8be0baa2436c60f8f9bdaf63425e112585db6
heptaliane <heptaliane@gmail.com>
2023-03-27 00:02:05 +0900

    Add Music Completed handler

--- app/src/main/java/com/example/jetmusic/JetMusicApp.kt
+++ app/src/main/java/com/example/jetmusic/JetMusicApp.kt
@@ -4,9 +4,27 @@ import android.app.Application
 import android.media.MediaPlayer
 import com.example.jetmusic.data.PlayList
 import com.example.jetmusic.data.PlayListManager
+import com.example.jetmusic.data.PlayListType
 
 class JetMusicApp : Application() {
     val player: MediaPlayer = MediaPlayer()
     val manager: PlayListManager = PlayListManager()
     var playList: PlayList = PlayList(emptyList())
+
+    init {
+        this.player.setOnCompletionListener {
+            this.playList.next()
+            this.reloadPlayerMusic()
+        }
+    }
+
+    private fun reloadPlayerMusic() {
+        this.player.seekTo(0)
+
+        val music = this.playList.get()
+        if (music != null) {
+            this.player.setDataSource(music)
+            this.player.start()
+        }
+    }
 }
